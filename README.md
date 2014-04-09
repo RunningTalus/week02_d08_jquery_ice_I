e18426dba27b5d48448ab99affcf0a91811bbf84
runningtalus <dave@davemay.org>
2014-04-09 12:33:58 -0600

    Heading to lunch, clean up last exercise and add directions

---
+++ README.md
@@ -0,0 +1,39 @@
+	//
+	jQuery Notes
+		jQuery Formula
+			$('Selector').method(argument, argument);
+			
+			$().css('color','red')
+				// $('.container').css('color', 'red');
+				// $('h1').css('color', 'red');
+				// $('p').css('color', 'red');
+			
+			$ sign is The jQuery Object/Function
+				$ is a jQuery function
+					('h1') is the selector AND a STRING AND an ARGUMENT
+						.css is a METHOD AND a FUNCTION
+							('color', is an ARGUMENT and a STRING
+							'red');, is an ARGUMENT and a STRING
+
+//sidebar
+	foo(10);
+		foo is a FUNCTION AND a FUNCTION OBJECT
+		foo(10); is a FUNCTION CALL
+
+
+POLYMORPHISM in jQuery
+	GET (GETTERS) & SET (SETTER)
+	Examples
+		.css
+		.text
+		.html
+		.width
+		.attr
+		.val
+		.offset
+		.addClass
+		.removeClass
+		.hasClass
+
+
+				
--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 $(document).on('ready', function() {
   
 });
+
+// $('.container').css('color', 'red');
+// $('h1').css('color', 'red');
+// $('p').css('color', 'red');
+
+$('h1').css('color');
+//CDT console returns "rgb(0,0,0)"
+//This is a GET/GETTER, it returns the curre
+$('h1').css('color', 'peachpuff');
+//This is a SETTER, when it receives two arguments it behaves differently.
+//This is an example of POLYMORPHISM.
+
+//In-class challenge II
+$('h1').css('color','red');
+$('h1').addClass('alert' 'alert-info');
+$('.container a:first-child').css('height', '100px');
+//
+
+$('.main-nav a:first-child').attr('href');
+// CDT "/exercises"
+
+$('.alert').html();
+//CDT "Welcome, RunningTalus!"
+console.log($('.alert').html());
+var extrStr = $('.alert').html();
+console.log(extrStr);
+var nomLength = extrStr.length
+console.log(nomLength);
+var nomSplit = extr.split(' ');
+//cdt ['Welcome,'' 'RunningTalus!']
+console.log(nom.split(' '));
+nomSplit[1]
+nomSplit.length
+name=nomSplit[1];
+name.length
+name.slice(0,12)
+
+
+
+
